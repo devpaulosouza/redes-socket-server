2339b06a8776cb2e6f8d2b01d7e7448cea0444d8
laurom <lauro.milagres@refspatial.com.br>
2019-05-26 13:38:16 -0300

    up

--- server.py
+++ server.py
@@ -1,32 +1,50 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-###################################################
-# File name: server.py                            #
-###################################################
-# author: Lauro Milagres Oliveira                 #
-# author: Paulo Souza                             #
-###################################################
+######################################################################################################
+# File name: server.py                                                                               #
+######################################################################################################
+# author: Lauro Milagres Oliveira                                                                    #
+# author: Paulo Souza                                                                                #
+######################################################################################################
 
 import socket
 
-class Jogo(Tabuleiro):
-    def __init__(self):
+
+######################################################################################################
+#                                               Class Jogo                                           #
+######################################################################################################
+
+class Jogo:
+    
+    def __init__(self,player1,player2):
+        self.player1 = player1
+        self.player2 = player2
+
+
+
+######################################################################################################
+#                                             Class Jogador                                          #
+######################################################################################################
+
+class Jogador(Tabuleiro):
+    
+    def __init__(self, id, nome):
         super().__init__(tabuleiro)
-        self.id = 0
+        self.id = id
+        self.nome = nome
 
 
-class Jogador:
-    def __init__(self):
-        self.id = 0
-        self.nome = 0
-        
+######################################################################################################
+#                                            Class Tabuleiro                                         #
+######################################################################################################
 
 class Tabuleiro:
+
     def __init__(self):
         self.tamanho_linhas = 10
         self.tamanho_colunas = 10
-        self.tabuleiro = 0
+        self.tabuleiro = self.Tabuleiro()
 
     def Tabuleiro(self):
         tabuleiro = []
@@ -37,29 +55,42 @@ class Tabuleiro:
             tabuleiro.append(board)
         return tabuleiro
 
-# Endereco do servidor
-HOST = ''
-# Porta do servidor
-PORT = input("Insira a porta: ")
+######################################################################################################
+#                                       Class Converte para Json                                     #
+######################################################################################################
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
-s.listen(1)
+class ConvertJson:
+    pass
 
-print('aguardando conexão...')
 
-while True:
-    conn, addr = s.accept()
-    print ('consagrado no ip ' + addr[0] + ' se conectou.' )
 
-    close = False
+######################################################################################################
 
-    while not close:
-        try:
-            data = conn.recv(1024)
-        except socket.error:
-            print ('Deu ruim...')
-        if data:
-            print (data.decode('utf-8'))
-            conn.send('data'.encode())
-            close = True
+if __name__ == "__main__":
+    
+    # Endereco do servidor
+    HOST = ''
+    # Porta do servidor
+    PORT = input("Insira a porta: ")
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((HOST, PORT))
+    s.listen(1)
+
+    print('aguardando conexão...')
+
+    while True:
+        conn, addr = s.accept()
+        print ('consagrado no ip ' + addr[0] + ' se conectou.' )
+
+        close = False
+
+        while not close:
+            try:
+                data = conn.recv(1024)
+            except socket.error:
+                print ('Deu ruim...')
+            if data:
+                print (data.decode('utf-8'))
+                conn.send('data'.encode())
+                close = True
