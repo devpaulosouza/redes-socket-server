98c7d50814a777f776135ee6e14f3e594fdc2184
laurom <lauro.milagres@refspatial.com.br>
2019-05-29 09:52:12 -0300

    verificacao

--- websocket.py
+++ websocket.py
@@ -37,10 +37,13 @@ class Jogo:
         self.attackedBlocksPlayer2 = []
 
 
-    def VerificaResultado():
-        pass
+    def VerificaResultado(self):
+        if len(reduce(lambda x,y : x+y, self.shipsPlayer1)) == len(self.attackedBlocksPlayer1):
+            return self.player2 , self.player1
+        if len(reduce(lambda x,y : x+y, self.shipsPlayer2)) == len(self.attackedBlocksPlayer2):
+            return self.player1 , self.player2
 
-
+        return None , None
     
     
     def VerificaPosicao(self,player,coodX,coodY):
